90a2b4e7bca381a28fadb72d59027466088c91e5
thechoreolab <thechoreolaboratory@gmail.com>
2025-10-26 17:24:09 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css"; // global styles
+import "./App.css"; // component styles
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
